2bb93825933e8eee7223460a6b07bd5e47b139e5
EldenBear <dontet1995@gmail.com>
2023-04-25 19:53:12 -0400

    Formatted code

--- index.js
+++ index.js
@@ -1,58 +1,68 @@
 // Imports used for logo
-const inquirer = require('inquirer');
-const fs = require('fs');
-const jest = require('jest');
-const {Circle} = require('./lib/shapes');
-const {Triangle} = require('./lib/shapes');
-const {Square} = require('./lib/shapes');
+const inquirer = require("inquirer");
+const fs = require("fs");
+const jest = require("jest");
+const { Circle } = require("./lib/shapes");
+const { Triangle } = require("./lib/shapes");
+const { Square } = require("./lib/shapes");
 // Prompts given to user
 const questions = [
-    {
-        type: 'input',
-        name: 'letters',
-        message: "Please enter up to three characters for your logo.",
-    },
+  {
+    type: "input",
+    name: "letters",
+    message: "Please enter up to three characters for your logo.",
+  },
 
-    {
-        type: 'input',
-        name: 'letterColor',
-        message: "Please enter a color for your characters on the logo.",
-    },
+  {
+    type: "input",
+    name: "letterColor",
+    message: "Please enter a color for your characters on the logo.",
+  },
 
-    {
-        type: 'list',
-        name: 'shape',
-        message: "Please choose a shape for your logo.",
-        choices: ['Circle', 'Triangle', 'Square'],
-    },
+  {
+    type: "list",
+    name: "shape",
+    message: "Please choose a shape for your logo.",
+    choices: ["Circle", "Triangle", "Square"],
+  },
 
-    {
-        type: 'input',
-        name: 'shapeColor',
-        message: "Please enter a color for your shape on the logo.",
-    },
+  {
+    type: "input",
+    name: "shapeColor",
+    message: "Please enter a color for your shape on the logo.",
+  },
 ];
 
 // Function that takes in user input and creates a svg file
 function init() {
-    inquirer
-    .prompt(questions)
-    .then((data) => {
-        let svgString = "";
-        if (data.shape === "Circle") {
-            const circleClass = new Circle(data.letters, data.letterColor, data.shapeColor);
-            svgString = circleClass.render();
-        } else if (data.shape === "Triangle") {
-            const triangleClass = new Triangle(data.letters, data.letterColor, data.shapeColor);
-            svgString = triangleClass.render();
-        } else {
-            (data.shape === "Square")
-            const squareClass = new Square(data.letters, data.letterColor, data.shapeColor);
-            svgString = squareClass.render();
-        };
-        const fileName = 'Logo.svg';
-         fs.writeFileSync(fileName, svgString, () => {});
-    });
-};
+  inquirer.prompt(questions).then((data) => {
+    let svgString = "";
+    if (data.shape === "Circle") {
+      const circleClass = new Circle(
+        data.letters,
+        data.letterColor,
+        data.shapeColor
+      );
+      svgString = circleClass.render();
+    } else if (data.shape === "Triangle") {
+      const triangleClass = new Triangle(
+        data.letters,
+        data.letterColor,
+        data.shapeColor
+      );
+      svgString = triangleClass.render();
+    } else {
+      data.shape === "Square";
+      const squareClass = new Square(
+        data.letters,
+        data.letterColor,
+        data.shapeColor
+      );
+      svgString = squareClass.render();
+    }
+    const fileName = "Logo.svg";
+    fs.writeFileSync(fileName, svgString, () => {});
+  });
+}
 // Calls init function
 init();
